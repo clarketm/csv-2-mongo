d9b00b381f39c0e0b76c73feaca3de6cd151211f
Travis Clarke <travis.m.clarke@gmail.com>
2018-12-10 16:06:11 -0800

    init setup.py and requirements.txt

--- setup.py
+++ setup.py
@@ -1,26 +1,27 @@
 import setuptools, os
 
-from pprintast.pprintast import __VERSION__
+from csv_2_mongo import __VERSION__
 
 with open(f"{os.path.abspath(os.path.dirname(__file__))}/README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="pprintast",
+    name="csv-2-mongo",
     version=__VERSION__,
-    author="Travis Clarke, Alex Leone",
-    author_email="travis.m.clarke@gmail.com, acleone@gmail.com",
-    description="A AST pretty printer for python.",
+    author="Travis Clarke",
+    author_email="travis.m.clarke@gmail.com",
+    description="Import a CSV to MongoDB.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/clarketm/pprintast",
+    url="https://github.com/clarketm/csv_2_mongo",
     packages=setuptools.find_packages(),
-    py_modules=["pprintast"],
-    entry_points={"console_scripts": ["pprintast=pprintast.pprintast:cli"]},
+    install_requires=["click", "pandas", "pymongo"],
+    extras_require={"dev": ["black", "nose2"]},
+    py_modules=["csv_2_mongo"],
+    entry_points={"console_scripts": ["csv-2-mongo=csv_2_mongo:cli"]},
     classifiers=[
         "Intended Audience :: Developers",
         "Natural Language :: English",
-        "Programming Language :: Python :: 2",
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
